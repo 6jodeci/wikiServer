1d5ed5f13126cce9af16e5aa9392fcea59de33a2
666savages <egor0263s@gmail.com>
2021-10-07 00:50:22 +0500

    wiki

---
+++ wiki.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"html/template"
+	"os"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func main() {
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/shutdown/", shutdown) // шотдаун сервера
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{
+		Title: "hello",
+		Body:  body,
+	}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil { // если пользователь переходит на несуществующую страницу, то отправлять на edit
+		http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+		return
+	}
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+func shutdown(res http.ResponseWriter, req *http.Request) {
+	os.Exit(0) // немедленно завершает приложение
+}
